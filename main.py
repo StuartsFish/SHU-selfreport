24b2ebd33dd0ce846da32fc9d703f6e2f40aa356
Junyi Chen <jy.chen53@outlook.com>
2021-11-02 14:21:01 +0800

    ok

--- main.py
+++ main.py
@@ -7,11 +7,7 @@ from pathlib import Path
 import yaml
 from bs4 import BeautifulSoup
 
-<<<<<<< HEAD
-from fstate_generator import generate_fstate_day, get_last_report
-=======
 from fstate_generator import generate_fstate_day, get_last_report, get_img_value
->>>>>>> b429ab9940ff52353bc0985c3da6930aeddaec6f
 from login import login
 
 NEED_BEFORE = False  # 如需补报则置为True，否则False
@@ -65,20 +61,12 @@ def report_day(sess, t):
 
     BaoSRQ = t.strftime('%Y-%m-%d')
     ShiFSH, ShiFZX, ddlSheng, ddlShi, ddlXian, XiangXDZ, ShiFZJ = get_last_report(sess, t)
-<<<<<<< HEAD
-#     SuiSM, XingCM = get_img_value(sess)
-=======
     SuiSM, XingCM = get_img_value(sess)
->>>>>>> b429ab9940ff52353bc0985c3da6930aeddaec6f
 
     print(f'是否在上海：{ShiFSH}', f'是否在校：{ShiFZX}')
     print(ddlSheng, ddlShi, ddlXian, f'###{XiangXDZ[-2:]}')
     print(f'是否为家庭地址：{ShiFZJ}')
-<<<<<<< HEAD
-#     print(f'随身码：{SuiSM}，行程码：{XingCM}')
-=======
     print(f'随身码：{SuiSM}，行程码：{XingCM}')
->>>>>>> b429ab9940ff52353bc0985c3da6930aeddaec6f
 
     for _ in range(RETRY):
         try:
@@ -138,13 +126,8 @@ def report_day(sess, t):
                 "p1_ContentPanel1_Collapsed": "true",
                 "p1_GeLSM_Collapsed": "false",
                 "p1_Collapsed": "false",
-<<<<<<< HEAD
-#                 "p1$pImages$HFimgSuiSM": SuiSM,
-#                 "p1$pImages$HFimgXingCM": XingCM,
-=======
                 "p1$pImages$HFimgSuiSM": SuiSM,
                 "p1$pImages$HFimgXingCM": XingCM,
->>>>>>> b429ab9940ff52353bc0985c3da6930aeddaec6f
                 "F_STATE": generate_fstate_day(BaoSRQ, ShiFSH, ShiFZX,
                                                ddlSheng, ddlShi, ddlXian, XiangXDZ, ShiFZJ,
                                                SuiSM, XingCM)
